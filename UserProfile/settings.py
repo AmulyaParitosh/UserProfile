bc4b6cd22b99982ac3811456fe6803f5ebe902b8
Amulya Paritosh <amulya1171@gmail.com>
2024-11-10 01:51:19 +0530

    FIX:fixed redirection to login page if unauthenticated user

--- UserProfile/settings.py
+++ UserProfile/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "/login/"
+
 
 # default backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
